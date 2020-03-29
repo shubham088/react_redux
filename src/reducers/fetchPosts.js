02e51ee0fbb8114c2e9e596ff689365b7a2152ef
shubham088 <shubhamsharma0296@gmail.com>
2020-03-30 01:38:02 +0530

    Update fetchPosts.js

--- src/reducers/fetchPosts.js
+++ src/reducers/fetchPosts.js
@@ -3,26 +3,7 @@ const initialState = {
  isFetching : false,
  posts :  []
 }
-// {
-//    "userId": 1,
-//    "id": 1,
-//    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-//    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-//  }
 
-// const fetchPostReducer = (state=initialState, action) => {
-//   console.log("fetch reducer fired..");
-//   switch(action.type){
-//     case 'FETCH_POSTS' :
-//     console.log("payload : ",action.payload.data);
-//       return {
-//         ...state,
-//          posts : [...state.posts, ...action.payload.data]
-//       }
-//     default :
-//      return initialState;
-//   }
-// }
 
 const fetchPostReducer = (state = initialState, action) => {
   console.log("fetch reducer fired ");
